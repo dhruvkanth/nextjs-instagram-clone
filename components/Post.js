1a99d527c4fd33798a80f37873c1b2f3c5a5388e
dhruvkanth <100739087+dhruvkanth@users.noreply.github.com>
2022-09-09 08:53:08 +1200

    feat: added buttons to posts section

--- components/Post.js
+++ components/Post.js
@@ -1,14 +1,24 @@
-import { DotsHorizontalIcon } from "@heroicons/react/outline";
+import { DotsHorizontalIcon, HeartIcon, ChatIcon, BookmarkIcon } from "@heroicons/react/outline";
 
 const Post = ({ img, userImg, caption, username, id }) => {
   return (
     <div className="bg-[#000000] text-white mt-7 border rounded-md">
+
       <div className="flex items-center p-5">
         <img className="h-12 rounded-full object-cover border p-1 mr-3" src={userImg} alt={username} />
         <p className="font-bold flex-1">{username}</p>
         <DotsHorizontalIcon className="h-5" />
       </div>
+
       <img className="object-fill w-full" src={img} alt="" />
+
+      <div className="flex justify-between px-4 py-4">
+        <div className="flex space-x-4">
+          <HeartIcon className="btn" />
+          <ChatIcon className="btn" />
+        </div>
+        <BookmarkIcon className="btn" />
+      </div>
     </div>
   );
 }
