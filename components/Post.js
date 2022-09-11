1e7ed68882a10248aff9ab4acb9a489cd2bfde54
dhruvkanth <100739087+dhruvkanth@users.noreply.github.com>
2022-09-11 15:11:43 +1200

    feat: added like functionality

--- components/Post.js
+++ components/Post.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
-import Moment from 'react-moment';
+import Moment from "react-moment";
 import { db } from "../firebase";
 import { DotsHorizontalIcon, HeartIcon, ChatIcon, BookmarkIcon, EmojiHappyIcon } from "@heroicons/react/outline";
-import { addDoc, collection, serverTimestamp, onSnapshot, orderBy, query } from "firebase/firestore";
+import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
+import { addDoc, collection, deleteDoc, doc, onSnapshot, orderBy, query, serverTimestamp, setDoc } from "firebase/firestore";
 import { useSession } from "next-auth/react";
 
 const Post = ({ img, userImg, caption, username, id }) => {
@@ -12,11 +13,34 @@ const Post = ({ img, userImg, caption, username, id }) => {
   const [comment, setComment] = useState("");
 
   const [comments, setComments] = useState([]);
+
+  const [likes, setLikes] = useState([]);
+
+  const [hasLiked, setHasLiked] = useState(false);
+
   useEffect(() => {
     const unsubscribe = onSnapshot(
       query(collection(db, "posts", id, "comments"), orderBy("timestamp", "desc")), (snapshot) => { setComments(snapshot.docs) }
     )
   }, [db, id])
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(collection(db, "posts", id, "likes"), (snapshot) => { setLikes(snapshot.docs) });
+  }, [db]);
+
+  useEffect(() => {
+    setHasLiked(likes.findIndex((like) => like.id === session?.user.name) !== -1);
+  }, [likes]);
+
+  async function likePost() {
+    if (hasLiked) {
+      await deleteDoc(doc(db, "posts", id, "likes", session.user.name));
+    } else {
+      await setDoc(doc(db, "posts", id, "likes", session.user.name), {
+        username: session.user.name,
+      });
+    }
+  }
 
   async function sendComment(event) {
     event.preventDefault();
@@ -44,14 +68,28 @@ const Post = ({ img, userImg, caption, username, id }) => {
       {session &&
         <div className="flex justify-between px-4 py-4">
           <div className="flex space-x-4">
-            <HeartIcon className="btn" />
+            {hasLiked ? (
+              <HeartIconFilled
+                onClick={likePost}
+                className="text-red-400 btn"
+              />
+            ) : (
+              <HeartIcon onClick={likePost} className="btn" />
+            )}
             <ChatIcon className="btn" />
           </div>
           <BookmarkIcon className="btn" />
         </div>
       }
 
+      {likes.length > 1 ? (
+        <p className="font-bold mx-5 my-3">{likes.length} likes</p>
+      ) : (
+        <p className="font-bold mx-5 my-3">{likes.length} like</p>
+      )}
+
       <p className="p-5 truncate"><span className="font-bold mr-2">{username}</span>{caption}</p>
+
       {comments.length > 0 && (
         <div className="mx-10 max-h-24 overflow-y-scroll scrollbar-none">
           {comments.map(comment => (
